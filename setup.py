a2df0374c40d437f33db3e006fdaeaf6ee1a27a4
hamza38954017 <hamza38954017@gmail.com>
2026-03-15 05:36:15 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='xhamza',
+    version='1.0.0', # Change this if you update your code later
+    description='A simple tool to extract m3u8  links of xhamster video.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Hamza',
+    packages=find_packages(),
+    install_requires=[
+        'curl_cffi>=0.5.10'
+    ],
+    entry_points={
+        'console_scripts': [
+            'xhamza=xhamza.core:main',
+        ],
+    },
+)
